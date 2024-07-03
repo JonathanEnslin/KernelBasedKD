7a3270e05f995b93f411c2e158eb79cbd9eda881
JonathanEnslin <jonathan.enslin112@gmail.com>
2024-07-04 00:15:12 +0200

    Added code sourced for M. Dickson's repo on Filter Based KD

---
+++ functions/loss/loss_type.py
@@ -0,0 +1,14 @@
+'''
+Sourced from https://github.com/Matthew-Dickson/FilterBasedKnowledgeDistillation/tree/main
+'''
+
+from enum import Enum
+ 
+class LossType(Enum):
+    FILTER = 1
+    TRADITIONAL = 2
+    ATTENTION = 3
+    CE = 4
+    FEATURE = 5
+    FILTER_LAST = 6
+    FEATURE_LAST = 7
